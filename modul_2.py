df4dda1b1155732e50947f202f13b8ca9005e207
nailul-ath <nathiyyah2105@gmail.com>
2025-03-11 18:15:28 +0700

    modul_2.py

--- modul_2.py
+++ modul_2.py
@@ -2,7 +2,7 @@ print ('Halo! selamat datang di math cafe, \nsilakan pilih paket pesanan anda!')
 paket_1 = 'ayam = 20000'
 paket_2 = 'sapi = 35000'
 paket_3 = 'cumi-cumi = 45000'
-print (f'paket 1 = {paket_1} \npaket 2 = {paket_2} \npaket 3 = {paket_3}')
+print (f'paket 1 : {paket_1} \npaket 2 : {paket_2} \npaket 3 : {paket_3}')
 pilihan= int(input('pilihan paket pesanan anda [1/2/3]:   '))
 jumlah= int(input('jumlah pesanan anda:'))
 if pilihan == 1:
@@ -17,7 +17,7 @@ else:
 total= harga*jumlah
 print (f'total harga pesanan anda adalah: {total}')
 #ongkos kirim
-jarak = int(input('jarak rumah anda ke restoran(km):   '))
+jarak = float(input('jarak rumah anda ke restoran(km):   '))
 if jarak<1:
     ongkir = 0
 elif 1<=jarak<=3:
